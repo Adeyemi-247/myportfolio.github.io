1084b7da7d175795aa0547cde0b210a4bcceb65f
Adeyemi Abiodun <samuelabiodun377@gmail.com>
2023-08-12 15:50:11 +0530

    real portfolio

--- index.js
+++ index.js
@@ -6,22 +6,29 @@ const bodys = document.querySelector("body");
 
 
 
-myMenuBar.onclick = function(){
-    navLinks.style.transform = "translateX(0)";
+myMenuBar.onclick = function () {
+    navLinks.style.transform = "translateY(0)";
     myMenuBar.style.display = "none";
     myCloseBar.style.display = "block";
-    
+
 }
 
-myCloseBar.onclick = function(){
-    navLinks.style.transform ="translateX(110%)";
+myCloseBar.onclick = function () {
+    navLinks.style.transform = "translateY(-160%)";
     myMenuBar.style.display = "block";
     myCloseBar.style.display = "none";
-    
+
 }
+
+// bodys.onscroll = function () {
+//     navLinks.style.transform = "translateY(-160%)";
+//     myMenuBar.style.display = "block";
+//     myCloseBar.style.display = "none";
+
+// }
 // helloME.onclick = function (){
 //     navLinks.style.transform ="translateX(110%)";
-    
+
 // }
 // bodys.onscroll = function (){
 //     navLinks.style.transform ="translateX(110%)";
@@ -36,26 +43,26 @@ myCloseBar.onclick = function(){
 
 ScrollReveal({
     reset: true,
-    distance:'60px',
-    duration:2500,
-    delay:400
+    distance: '60px',
+    duration: 2500,
+    delay: 400
 });
-ScrollReveal().reveal('.mine2',  { delay: 500, origin:'left', distance:'70%' });
-ScrollReveal().reveal('.deimg',  { delay: 500, origin:'bottom', distance:'70%' });
-ScrollReveal().reveal('.zazu',  { delay: 500, origin:'left', distance:'70%' });
-ScrollReveal().reveal('.desimg',  { delay: 500, origin:'right', distance:'70%' });
-ScrollReveal().reveal('.dotman',  { delay: 500, origin:'bottom', distance:'70%' });
-ScrollReveal().reveal('.prog2',  { delay: 500, origin:'right', distance:'70%' });
-ScrollReveal().reveal('.dongs',  { delay: 500, origin:'bottom', distance:'70%' });
-ScrollReveal().reveal('.klop1',  { delay: 500, origin:'left', distance:'70%' });
-ScrollReveal().reveal('.klop2',  { delay: 500, origin:'top', distance:'70%' });
-ScrollReveal().reveal('.klop3',  { delay: 500, origin:'right', distance:'70%' });
-ScrollReveal().reveal('.klop4',  { delay: 500, origin:'top', distance:'70%' });
-ScrollReveal().reveal('.klop5',  { delay: 500, origin:'bottom', distance:'70%' });
-ScrollReveal().reveal('.klop6',  { delay: 500, origin:'top', distance:'70%' });
-ScrollReveal().reveal('.dedesi',  { delay: 500, origin:'left', distance:'50%' });
-ScrollReveal().reveal('.prok',  { delay: 500, origin:'right', distance:'50%' });
-ScrollReveal().reveal('.proj2',  { delay: 500, origin:'left', distance:'50%' });
+ScrollReveal().reveal('.mine2', { delay: 500, origin: 'left', distance: '70%' });
+ScrollReveal().reveal('.deimg', { delay: 500, origin: 'bottom', distance: '70%' });
+ScrollReveal().reveal('.zazu', { delay: 500, origin: 'left', distance: '70%' });
+ScrollReveal().reveal('.desimg', { delay: 500, origin: 'right', distance: '70%' });
+ScrollReveal().reveal('.dotman', { delay: 500, origin: 'bottom', distance: '70%' });
+ScrollReveal().reveal('.prog2', { delay: 500, origin: 'right', distance: '70%' });
+ScrollReveal().reveal('.dongs', { delay: 500, origin: 'bottom', distance: '70%' });
+ScrollReveal().reveal('.klop1', { delay: 500, origin: 'left', distance: '70%' });
+ScrollReveal().reveal('.klop2', { delay: 500, origin: 'top', distance: '70%' });
+ScrollReveal().reveal('.klop3', { delay: 500, origin: 'right', distance: '70%' });
+ScrollReveal().reveal('.klop4', { delay: 500, origin: 'top', distance: '70%' });
+ScrollReveal().reveal('.klop5', { delay: 500, origin: 'bottom', distance: '70%' });
+ScrollReveal().reveal('.klop6', { delay: 500, origin: 'top', distance: '70%' });
+ScrollReveal().reveal('.dedesi', { delay: 500, origin: 'left', distance: '50%' });
+ScrollReveal().reveal('.prok', { delay: 500, origin: 'right', distance: '50%' });
+ScrollReveal().reveal('.proj2', { delay: 500, origin: 'left', distance: '50%' });
 
 
 
